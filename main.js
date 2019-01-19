87d979dc3718f4a5d2a7d2de0b18c5d6a5604dc7
Daniel Flores <d.flores042@gmail.com>
2019-01-19 16:31:48 -0500

    implement flexbox styling

--- main.js
+++ main.js
@@ -2,12 +2,9 @@
 
 var modal = document.querySelector('#simpleModal');
 var modalBtn = document.querySelector('#modalBtn');
-var closeBtn = document.querySelector('.closeBtn');
 
 // listen for open click
 modalBtn.addEventListener('click', openModal);
-// listen for close click
-closeBtn.addEventListener('click', closeModal);
 // listen for outside click
 window.addEventListener('click', clickOutside);
 
@@ -16,12 +13,9 @@ function openModal(){
   modal.style.display = 'block';
 }
 
-function closeModal(){
-  modal.style.display = 'none';
-}
-
 function clickOutside(e){
   if(e.target == modal){
     modal.style.display = 'none';
+    modalBtn.style.display = 'block';
   }
 }
--- window_functions.js
+++ window_functions.js
@@ -1,14 +1,14 @@
-var onWindowResize = (e) => {
-  let width = e.target.outerWidth;
-  let navWidth = document.querySelector('.navbar')
-  console.log(navWidth);
-  if(width >= 769) {
-    //remove alert just added for debug
-    navWidth.style.maxWidth = "1030px";
-  } else{
-    //remove alert
-    navWidth.style.maxWidth = "720px";
-  }
-}
+// var onWindowResize = (e) => {
+//   let width = e.target.outerWidth;
+//   let navWidth = document.querySelector('.navbar')
+//   console.log(navWidth);
+//   if(width >= 769) {
+//     //remove alert just added for debug
+//     navWidth.style.maxWidth = "1030px";
+//   } else{
+//     //remove alert
+//     navWidth.style.maxWidth = "720px";
+//   }
+// }
 
-window.addEventListener("resize", onWindowResize);
+// window.addEventListener("resize", onWindowResize);
